34ae773ae98e0343a9ba6a8827953121a4e8c605
IvanIsak2000 <vingving648@gmail.com>
2024-01-22 00:54:55 +0300

    add model

--- src/Restaurant/app1/models.py
+++ src/Restaurant/app1/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class Pizza(models.Model):
+    pizza_id = models.IntegerField(primary_key=True, unique=True, null=False)
+    pizza_name = models.CharField(unique=True, null=False, max_length=30)
+    pizza_price = models.FloatField(unique=False, null=False)
+    pizza_ingredients = models.ArrayField(CharField(max_length=100,null=False))
